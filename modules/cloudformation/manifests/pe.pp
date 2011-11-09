d8b7f14c7bc8cffd1d2ff94e393ccba6a3d50745
Dan Bode <dan@puppetlabs.com>
2011-11-09 09:49:02 -0800

    (#10597) Add code to download modules from forge
    This commit adds code so that you can specify
    what modules you want to download to the puppet
    master as a part of your cloud foration stack.
    
    Adds the install_modules parameter that accepts
    a list of modules to download from the forge.
    
    Downloads the dashboard face prototype from github.
    
    Uses the dashboard face to download the specified
    modules from the forge.

--- modules/cloudformation/manifests/pe.pp
+++ modules/cloudformation/manifests/pe.pp
@@ -1,5 +1,10 @@
   $puppet_agents
+#
+#
+# [*install_modules*] List of modules to install from forge. Modules must be of the 
+#   form <module_owner_namespact>/<module_name>
 define cloudformation::pe(
+  $install_modules = []
 ) {
   file { $name:
     content => template('cloudformation/pe.erb')
