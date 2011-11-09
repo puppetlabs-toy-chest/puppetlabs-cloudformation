54de9c1315d32368def63a2158982a4f47c633b3
Dan Bode <dan@puppetlabs.com>
2011-11-09 09:54:51 -0800

    (#10591) Add ability to classify nodes to cfn template
    Modifies puppet_agents to accept a hash instead
    of an array where the hash values are hashes
    representing the classes to be assigned to the
    node.
    
    Places the classification hashes in the Puppet meta
    data for the nodes that need to be classified.
    
    Adds an external node classifier to the ext directory.
    
    Downloads the external node classifier to the 
    master ec2 instance.
    
    stores the iam user credentials needed by the
    external node classifier on the master
    
    creates facts for the agents to represent stack_name
    and resource_id (these facts are required for the
    external node classifier)

--- modules/cloudformation/manifests/pe.pp
+++ modules/cloudformation/manifests/pe.pp
@@ -1,9 +1,12 @@
-  $puppet_agents
 #
 #
 # [*install_modules*] List of modules to install from forge. Modules must be of the 
 #   form <module_owner_namespact>/<module_name>
+# [*puppet_agents*] - List of resources names of agents to create in the stack.
+#   these agents also accept a hash of param classes to be set.
+#
 define cloudformation::pe(
+  $puppet_agents = {},
   $install_modules = []
 ) {
   file { $name:
