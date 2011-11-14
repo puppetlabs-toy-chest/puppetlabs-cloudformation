80387662c07f4aacc37caa7943814b9daa664583
Dan Bode <dan@puppetlabs.com>
2011-11-14 10:58:15 -0800

    (#10784) documentation improvements
    This commit contains the following documentation
    improvements.
    
    Add manifest docs to init.pp for cloudformation
    installer manifest.
    
    Reorders class parameters so that the most likely
    parameters to be set appear first.
    
    updates install.pp example manifest to better express
    that the AWS secret key should be provided.
    
    improves documentation in the README. Documents how
    to use the cloudformation face.

--- examples/install.pp
+++ examples/install.pp
@@ -1,4 +1,4 @@
 class { 'cloudformation':
-  aws_access_key => '< your key here >',
+  aws_access_key => '< your access key here >',
   aws_secret_key => '< your secret key here >'
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,43 @@
+# Class: cloudformation
+#   This class installs the cloudformation client tools and
+#   configures the cloudformation face.
+#
+# Tested Platforms:
+#  - Darwin 10.3.2
+#
+# Requirements:
+#  Assumes that Java version 1.5 or higher is installed.
+#
+# Parameters:
+#  [aws_access_key]
+#    *Required* AWS account credentials access key.
+#  [aws_secret_key]
+#    *Required* AWS account secret key.
+#  [java_home]
+#    *Optional* Location of java tools. Defaults to location for Mac.
+#  [cfn_version]
+#    *Optional* Version of the cloudformation client tools installed.
+#    Defaults to 1.0.9
+#  [base_dir]
+#    *Optional* Location where all of the generated files and directories will
+#    be installed. Defaults to the cloudformation module directory.
+#  [aws_credential_file]
+#    *Optional* Location where generated aws_credential_file should be saved.
+#
+# Actions:
+#   - installs and unzips cloudformation client tools
+#   - creates an example bashrc file that can be used to configure project.
+#   - creates a credential file
+#
 class cloudformation(
   $aws_access_key,
   $aws_secret_key,
-  $base_dir = get_module_path($module_name),
-  $aws_credential_file = "${base_dir}/aws_credentials",
   $java_home = '/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home',
-  $cfn_version = '1.0.9'
+  $cfn_version = '1.0.9',
+  $base_dir = get_module_path($module_name),
+  $aws_credential_file = "${base_dir}/aws_credentials"
 ) {
+
   exec { 'download_cloudformation_client':
     command => '/usr/bin/curl -o AWSCloudFormation-cli.zip https://s3.amazonaws.com/cloudformation-cli/AWSCloudFormation-cli.zip',
     cwd => $base_dir,
