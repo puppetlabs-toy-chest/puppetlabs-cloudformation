58f864f0062bdb7b165f2bbec02a96a091abe83a
Dan Bode <dan@puppetlabs.com>
2011-11-19 17:31:13 -0800

    (#10960) Extend group support
    Fixes a misspelled variable name so that the groups
    data actually gets compiled into the template as
    metadata for the master.
    
    Create the sync_group script which is used to query
    dashbaord group information out of the master's
    ec2 meta data and use it to populate the dashboard.
    
    Add code so that the master will download the
    sync_group script during bootstrapping.
    
    Add code so that the sync_group script is invoked on
    the master during bootstrapping.

---
+++ ext/sync_group.rb
@@ -0,0 +1,22 @@
+#!/opt/puppet/bin/ruby
+require 'puppet'
+require 'puppet/face'
+$LOAD_PATH.push('/etc/puppetlabs/puppet/modules/puppetlabs-dashboard/site_lib')
+# it needs three arguments
+group_meta_data=PSON.parse(File.read('/var/lib/cfn-init/data/metadata.json'))
+puts group_meta_data.inspect
+if group_meta_data.is_a?(Hash) and group_meta_data['Dashboard'].is_a?(Hash)
+  group_data = group_meta_data['Dashboard']['Groups']
+  (group_data || {}).each do |k,v|
+    Puppet::Face[:dashboard, :current].create_group(
+      :enc_server => 'localhost',
+      :enc_port => 443,
+      :enc_auth_passwd => 'cfn_password',
+      :enc_auth_user => 'cfn_user',
+      :enc_ssl => true,
+      :classes => v['classes'],
+      :parameters => v['parameters'],
+      :name => k
+    )
+  end
+end
--- lib/puppet/face/cloudformation.rb
+++ lib/puppet/face/cloudformation.rb
@@ -70,7 +70,7 @@ Puppet::Face.define(:cloudformation, '0.0.1') do
       if config.is_a?(Hash)
         install_modules = config['install_modules'] if config['install_modules']
         puppet_agents = config['puppet_agents'] if config['puppet_agents']
-        dasboard_groups = config['dashboard_groups'] if config['dashboard_groups']
+        dashboard_groups = config['dashboard_groups'] if config['dashboard_groups']
       end
 
       erb_template_file = Puppet::CloudFormation.get_pe_cfn_template
